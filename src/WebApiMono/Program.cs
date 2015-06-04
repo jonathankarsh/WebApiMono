0cdc84fff94f709e2f3cd116c4b64b3a8a89b6b1
Jonathan Karsh <jonathan.karsh@nearmap.com>
2015-06-04 16:07:45 +1000

    simple C# (.net 4.5) console project, referencing Microsoft.AspNet.WebApi.OwinSelfHost
        with some code from http://www.asp.net/web-api/overview/hosting-aspnet-web-api/use-owin-to-self-host-web-api
        and then some more to mimic overriding IHttpControllerTypeResolver ~ https://github.com/IdentityServer/IdentityServer3/commit/da685dfd3fd4937396c1191830cfc50a4ef634a6#diff-eabf679724b1db39740d70ebbc8145d9R40

---
+++ src/WebApiMono/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace WebApiMono {
+    class Program {
+        static void Main(string[] args) {
+            const string listenAddress = "http://*:9000/";
+            const string sendAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(listenAddress)) {
+                Console.WriteLine("Server started listening at {0}", listenAddress);
+
+                // Create HttpCient and make a request to api/values 
+                var client = new HttpClient();
+
+                var response = client.GetAsync(sendAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
---
+++ src/WebApiMono/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using Owin;
+using WebApiMono.Controllers;
+
+namespace WebApiMono {
+    public class Startup {
+        public void Configuration(IAppBuilder appBuilder) {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.SuppressDefaultHostAuthentication();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Services.Replace(typeof(IHttpControllerTypeResolver), new HttpControllerTypeResolver());
+            appBuilder.UseWebApi(config);
+        }
+
+        private class HttpControllerTypeResolver : IHttpControllerTypeResolver {
+            public ICollection<Type> GetControllerTypes(IAssembliesResolver assembliesResolver) {
+                var httpControllerType = typeof (IHttpController);
+                var controllerTypes = typeof(ValuesController)
+                    .Assembly
+                    .GetTypes()
+                    .Where(t => t.IsClass && !t.IsAbstract && httpControllerType.IsAssignableFrom(t))
+                    .ToList();
+                return controllerTypes;
+            }
+        }
+    }
+}
